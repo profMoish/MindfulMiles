477d04af8120e46953d831292bc0685a814a2bd6
Mr Moısh <v.y.moish@gmail.com>
2025-05-26 13:32:30 -0300

    chore: webhook

--- bot.js
+++ bot.js
@@ -1,126 +1,61 @@
-const TelegramBot = require('node-telegram-bot-api');
+const express = require("express");
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const app = express();
+app.use(express.json());
 
-const bot = new TelegramBot(token, { polling: true});
-
-
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const CHAT_ID = 1825527066; // Можно получить у @userinfobot в Telegram
 const reminders = [];
 
-bot.on('message', (msg) => {
-  reminders.push(msg.text)
-  console.log(msg.text)
-  bot.sendMessage(1825527066, '✅', {
-    disable_notification: true,
-    reply_markup: {
-      inline_keyboard: [[
-        { text: "🎯 Рандомная цель", callback_data: 'random_goal' }
-      ]]
+app.post(`/webhook/${TOKEN}`, (req, res) => {
+  const update = req.body;
+  
+  if (update.message) {
+    reminders.push(update.message.text);
+    const chatId = update.message.chat.id;
+    const text = update.message.text;
+    console.log(`Message from ${chatId}: ${text}`);
+
+  }
+
+  res.sendStatus(200);
+
+  const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: reminders[Math.floor(Math.random() * reminders.length)],
+          reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🎯 Рандомная цель",
+            callback_data: "random_goal"
+          }
+        ]
+      ]
     }
+    }),
+  })
+  .then(res => res.json())
+  .then(json => {
+    console.log('Ответ Telegram:', json);
+  })
+  .catch(err => {
+    console.error('Ошибка:', err);
   });
+
+
 });
 
+const port = 3000;
 
-// Обработка кнопки "🎯 Рандомная цель"
-bot.on('callback_query', (callbackQuery) => {
-  const msg = callbackQuery.message;
-  const goal = reminders[Math.floor(Math.random() * reminders.length)];
-  bot.sendMessage(msg.chat.id, `🎯 Цель: ${goal}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+
 });
-
-
-
-  
-
-
-const now = new Date();
-const h = now.getHours();
-const m = now.getMinutes();
-const s = now.getSeconds();
-console.log(`🚀 SCRIPT: ${h}:${m}:${s}`);
-
-let dayOfWeek = now.getDay();
-
-const timeAlarm = [36000, 28800, 19800, 23400, 28800, 21000, 34200]
-const diffAlarm = [2000, 3000, 4000, 5000, 10000, 9000, 8000]
-
-const allMinutes = h*60+m
-
-
-
-if (allMinutes >= timeAlarm[dayOfWeek]){
-  dayOfWeek += 1
-  var timeout = ((1439-allMinutes)*60 + timeAlarm[dayOfWeek] + 60-s);// 1439 это все минуты в 23 часах +59 минут
-}else
-  var timeout = ((timeAlarm[dayOfWeek] - allMinutes-1)*60 + 60-s);  
-
-console.log(timeout)
-// пн
-setTimeout(() => {
-  bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-  setInterval(() =>
-    bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-  , 604800000) // 604800 секунд в неделе
-  
-  //вт
-  setTimeout(() => {
-    bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-    setInterval(() =>
-      bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-    , 604800000) // 604800 секунд в неделе
-  
-    //ср
-    dayOfWeek += 1
-   
-    setTimeout(() => {
-      bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-      setInterval(() =>
-        bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-      , 604800000) // 604800 секунд в неделе
-  
-
-      // чт
-      dayOfWeek += 1
-      
-      setTimeout(() => {
-        bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-        setInterval(() =>
-          bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-        , 604800000) // 604800 секунд в неделе
-  
-        // пт
-        dayOfWeek += 1
-        
-        setTimeout(() => {
-          bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-          setInterval(() =>
-            bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-          , 604800000) // 604800 секунд в неделе
-  
-          //сб
-          dayOfWeek += 1
-          
-          setTimeout(() => {
-            bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-            setInterval(() =>
-              bot.sendMessage(1825527066, reminders[Math.floor(Math.random() * reminders.length)])
-            , 604800000) // 604800 секунд в неделе
-  
-  
-            dayOfWeek += 1
-  
-          }, diffAlarm[dayOfWeek]);
-
-        }, diffAlarm[dayOfWeek]);
-
-      }, diffAlarm[dayOfWeek]);
-
-    }, diffAlarm[dayOfWeek]);
-
-  }, diffAlarm[dayOfWeek]);
-  
-}, timeout*1000);
-
-
 
 
